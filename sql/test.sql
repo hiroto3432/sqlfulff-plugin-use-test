733e71ca4b106e6ca757824a63338e6d85c106fe
hiroto3432 <hiroto.yamashita@sprocket.bz>
2022-10-29 16:27:45 +0900

    feat: support custom rule

--- sql/test.sql
+++ sql/test.sql
@@ -1,6 +1,6 @@
--- has not-null constraint and defalut value is not set (fail)
+-- has not-null constraint and defalut value is not set (pass)
 alter table tbl1
-add column col1 TEXT not null;
+add column col1 TEXT not null default '';
 
 -- has not-null constraint and defalut value is set (pass)
 alter table tbl2
@@ -11,15 +11,15 @@ alter table tbl3
 add column col1 TEXT default ''
 not null;
 
--- has not-null constraint and defalut value is null (fail)
+-- has not-null constraint and defalut value is null (pass)
 alter table tbl4
-add column col1 TEXT not null default null;
+add column col1 TEXT not null default 'null';
 
--- adding multiple Columns (fail:L20,L22)
+-- adding multiple Columns (pass)
 alter table tbl5
-add column col1 TEXT not null,
+add column col1 TEXT not null default '',
 add column col2 TEXT default '',
-add column col3 TEXT not null;
+add column col3 TEXT not null default '';
 
 -- create table (pass)
 create table tbl6 (
